993413c62f6c7534908c8360a990ebe4fe6d2b00
skmdnurujjaman <smnideal@gmail.com>
2022-06-23 16:14:55 +0530

    Error handling added

--- config/db.js
+++ config/db.js
@@ -1,15 +1,21 @@
+/** @format */
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
-  console.log(
-    `Mongo database connected on ${conn.connection.host}`.cyan.underline.bold
-  );
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log(
+      `Mongo database connected on ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.log("Db Connection failed!");
+  }
 };
 
 module.exports = connectDB;

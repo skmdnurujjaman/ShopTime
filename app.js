f7d319ce7dec09895b6c90e046223644b68510da
skmdnurujjaman <smnideal@gmail.com>
2022-06-25 14:41:25 +0530

    procfile added

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(unknownEndpoints);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,

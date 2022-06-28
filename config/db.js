e2d894cbd885391618a4b41dd6bf11bf783d9580
skmdnurujjaman <smnideal@gmail.com>
2022-06-28 09:58:55 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 /** @format */
 
 const mongoose = require("mongoose");
-
+const URI = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.iqvmc.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(URI);
     console.log(
       `Mongo database connected on ${conn.connection.host}`.cyan.underline.bold
     );

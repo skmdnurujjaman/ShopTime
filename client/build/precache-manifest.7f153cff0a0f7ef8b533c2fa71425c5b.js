cd09762fa359bcd66663f0ff59a5b027e768a24d
skmdnurujjaman <smnideal@gmail.com>
2022-06-29 07:56:05 +0530

    p

---
+++ client/build/precache-manifest.7f153cff0a0f7ef8b533c2fa71425c5b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c56135dd30047412f826cbdc86a3b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5e58077bdcd616de1a0",
+    "url": "/static/css/2.bb803c14.chunk.css"
+  },
+  {
+    "revision": "c1ea65a6f9c4d2056187",
+    "url": "/static/css/main.bfd0d943.chunk.css"
+  },
+  {
+    "revision": "b5e58077bdcd616de1a0",
+    "url": "/static/js/2.7c977aca.chunk.js"
+  },
+  {
+    "revision": "c8a9f93adbea2121be381cd6d2c120ae",
+    "url": "/static/js/2.7c977aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ea65a6f9c4d2056187",
+    "url": "/static/js/main.6431421f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6431421f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "31a737c539bf89313cd1b4d5f43883fd",
+    "url": "/static/media/confirmation.31a737c5.png"
+  }
+]);
